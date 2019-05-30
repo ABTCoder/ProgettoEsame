3cccf832178fe7efa53c6e7c65ae719c86097335
User <User@172.23.135.66>
2019-05-30 11:43:39 +0200

    PRIMO TEST DI LETTURA DELL'URL

--- src/ManageTasks/manage_tasks.java
+++ src/ManageTasks/manage_tasks.java
@@ -1,10 +1,48 @@
 package ManageTasks;
 
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class manage_tasks {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static String readAll(Reader rd) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    int cp;
+	    while ((cp = rd.read()) != -1) {
+	      sb.append((char) cp);
+	    }
+	    return sb.toString();
+	  }
 
-	}
+	  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	    InputStream is = new URL(url).openStream();
+	    try {
+	        BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	        String jsonText = readAll(rd);
+	        JSONObject json = new JSONObject(jsonText);
+	        return json;
+	    } finally {
+	        is.close();
+	    }
+	  }
+
+	  public static void main(String[] args) throws IOException, JSONException {
+		  System.setProperty("http.agent", "Chrome"); //QUESTA ISTRUZIONE RISOLVE: server returned http response code 403 for url
+	      JSONObject json = readJsonFromUrl("https://www.dati.gov.it/api/3/action/package_show?id=d75a0f5f-729b-4b3c-8c5a-f70e6ff112a2");
+		  //JSONObject json = new JSONObject(IOUtils.toString(new URL("https://graph.facebook.com/me"), Charset.forName("UTF-8")));
+	      System.out.println(json.toString());
+	      //System.out.println(json.get("id"));
+	  }
 
 }
