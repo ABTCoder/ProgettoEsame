cc208f83942a115d1739e5bafde79b34116cff20
User <User@LAPTOP-RITBKNFG>
2019-05-31 11:23:10 +0200

    Racchiuse le istruzioni di salvataggio del CSV in una funzione apparte: saveCSVFromUrl

--- src/ManageTasks/manage_tasks.java
+++ src/ManageTasks/manage_tasks.java
@@ -8,7 +8,6 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.io.FileWriter;
 
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -31,22 +30,25 @@ public class manage_tasks {
 		JSONObject json = new JSONObject(jsonText);
 		return json;
 	}
+	
+	public static void saveCSVFromUrl(String url) throws IOException {
+		URL dataUrl = new URL(url);
+	    FileWriter writer = new FileWriter("dataset.csv");
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(dataUrl.openStream(), Charset.forName("UTF-8")));
+	    String content = readAll(rd);
+	    writer.write(content);
+	    writer.close();
+	}
 
 	  public static void main(String[] args) throws IOException, JSONException {
 		  System.setProperty("http.agent", "Chrome"); //QUESTA ISTRUZIONE RISOLVE: server returned http response code 403 for url
 	      JSONObject json = readJsonFromUrl("https://www.dati.gov.it/api/3/action/package_show?id=d75a0f5f-729b-4b3c-8c5a-f70e6ff112a2");
 	      JSONObject result = json.getJSONObject("result");
 	      JSONArray resources = result.getJSONArray("resources");
-	      System.out.println(resources);
+	      JSONObject dataset = resources.getJSONObject(0); //Salva il primo elemento dell'JSONArray
 	      
-	      JSONObject dataset = resources.getJSONObject(0);
-	      URL dataUrl = new URL(dataset.get("url").toString());
+	      saveCSVFromUrl(dataset.getString("url"));
 	      
-	      FileWriter writer = new FileWriter("dataset.csv");
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(dataUrl.openStream(), Charset.forName("UTF-8")));
-	      String content = readAll(rd);
-	      writer.write(content);
-	   
 	  }
 
 }
